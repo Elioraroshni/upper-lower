9852c42b676ef2e967983ddc3148421d53b664a6
Elioraroshni <tanzimarahman13@gmail.com>
2019-10-08 01:27:25 -0700

    Add project files.

---
+++ task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give lower limit");
+            int lower = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Give upper limit");
+            int upper = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Numbers between Upper and Lower limits are");
+
+            for (int i = lower; i <= upper; i++)
+            {
+
+                Console.WriteLine(i);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
